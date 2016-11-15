8ea7f68645484521b73e70a9dc6858b0f268b2cd
FryderykG <eszkimos@gmail.com>
2016-11-15 16:15:50 +0100

    make corrections in URL's

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,8 @@ from gallery import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('gallery.urls', namespace='posts')),
+    url(r'', views.post_list, name='list'),
+    url(r'^gallery/', include('gallery.urls', namespace='posts')),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^about/', views.about, name='about'),
     url(r'^auth/logout/', views.logout_view, name='logout'),
