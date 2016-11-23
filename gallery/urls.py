3caa59778b883d3f459521f97c754dbb75886ea9
FryderykG <eszkimos@gmail.com>
2016-11-23 13:44:08 +0100

    change urlpatterns to include /year/month/day pattern

--- gallery/urls.py
+++ gallery/urls.py
@@ -20,9 +20,7 @@ from gallery import views
 urlpatterns = [
     url(r'^$', views.post_list, name='list'),
     url(r'^create/$', views.post_create, name='create'),
-    url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
-    url(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
-    url(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete, name='delete'),
-
-
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/delete/$', views.post_delete, name='delete'),
 ]
